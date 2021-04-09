4be4805240a41915e54e977f823a65656c3f14d5
Kacper Ruściński <ruscin13@gmail.com>
2021-04-09 21:33:17 +0200

    add first and second exercise

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable react/jsx-no-bind */
+
 import React from "react";
 
-import "./app.styles.scss";
+import AES from "crypto-js/aes";
+import CFBmode from "crypto-js/mode-cfb";
+import CTRmode from "crypto-js/mode-ctr";
+import ECBmode from "crypto-js/mode-ecb";
+import OFBmode from "crypto-js/mode-ofb";
 
+import {
+  Input,
+  MenuItem,
+  Grid,
+  Select,
+  TextareaAutosize,
+  Button,
+} from "@material-ui/core";
+
+import "./app.styles.scss";
 import { makeStyles } from "@material-ui/core/styles";
 
-import TextareaAutosize from "@material-ui/core/TextareaAutosize";
-import Select from "@material-ui/core/Select";
-import Grid from "@material-ui/core/Grid";
-import MenuItem from "@material-ui/core/MenuItem";
+const ECB = "ECB";
+const CFB = "CFB";
+const CTR = "CTR";
+const CBC = "CBC";
+const OFB = "OFB";
+const FONT_INPUT = "fontInput";
 
 const useStyles = makeStyles({
   root: {
@@ -22,24 +40,101 @@ const useStyles = makeStyles({
     margin: "10px",
     width: "330px",
     backgroundColor: "#D9D9D9",
-    fontSize: "15px",
+  },
+  input: {
+    width: "50px",
   },
 });
+
+const hexToAscii = (string) => {
+  const hex = string.toString();
+  let decodedString = "";
+  for (let i = 0; i < hex.length; i += 2) {
+    decodedString += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+  }
+
+  return decodedString;
+};
+
+const getMode = (mode) => {
+  switch (mode) {
+    case ECB:
+      return ECBmode;
+    case CFB:
+      return CFBmode;
+    case CTR:
+      return CTRmode;
+    case OFB:
+      return OFBmode;
+    default:
+      return "";
+  }
+};
 
 function App(props) {
   const classes = useStyles();
 
-  const [exercise, setExercise] = React.useState(1);
+  const [blockMode, setBlockMode] = React.useState(CBC);
   const [message, setMessage] = React.useState("");
   const [encryptedMessage, setEncryptedMessage] = React.useState("");
+  const [decryptedMessage, setDecryptedMessage] = React.useState("");
+  const [timeToEncrypt, setTimeToEncrypt] = React.useState(0);
+  const [fontSize, setFontSize] = React.useState(12);
+
+  const changeTexts = (text) => {
+    const key = "1234567890123456";
+
+    const encryptedMessage = AES.encrypt(
+      text,
+      key,
+      blockMode === CBC ? {} : { mode: getMode(blockMode) }
+    );
+    const decryptedMessage = hexToAscii(
+      AES.decrypt(
+        encryptedMessage,
+        key,
+        blockMode === CBC ? {} : { mode: getMode(blockMode) }
+      )
+    );
+
+    console.log(
+      AES.decrypt(
+        encryptedMessage,
+        key,
+        blockMode === CBC ? {} : { mode: getMode(blockMode) }
+      )
+    );
+
+    setMessage(text);
+    setEncryptedMessage(encryptedMessage.toString());
+    setDecryptedMessage(decryptedMessage);
+  };
+
+  const countTime = (receivedText) => {
+    let t0 = performance.now();
+    changeTexts(receivedText);
+    let t1 = performance.now();
+    setTimeToEncrypt(t1 - t0);
+  };
 
   const handleInputChange = (event) => {
-    setMessage(event.target.value);
-    const changedMessage = messageToChange + messageToChange;
-    setEncryptedMessage(changedMessage);
+    const receivedText = event.target.value;
+    switch (event.target.id) {
+      case FONT_INPUT:
+        setFontSize(Number(receivedText));
+        break;
+
+      default:
+        countTime(receivedText);
+        break;
+    }
   };
   const handleSelectChange = (event) => {
-    setExercise(event.target.value);
+    setBlockMode(event.target.value);
+    countTime(message);
+  };
+  const handleClick = () => {
+    countTime(message);
   };
 
   return (
@@ -58,31 +153,78 @@ function App(props) {
           <Select
             labelId="demo-simple-select-label"
             id="demo-simple-select"
-            value={exercise}
+            value={blockMode}
             onChange={handleSelectChange}
           >
-            <MenuItem value={1}>Zadanie 1</MenuItem>
-            <MenuItem value={2}>Zadanie 2</MenuItem>
-            <MenuItem value={3}>Zadanie 3</MenuItem>
+            <MenuItem value={CBC}>CBC</MenuItem>
+            <MenuItem value={ECB}>ECB</MenuItem>
+            <MenuItem value={OFB}>OFB</MenuItem>
+            <MenuItem value={CFB}>CFB</MenuItem>
+            <MenuItem value={CTR}>CTR</MenuItem>
           </Select>
+          <span> </span>
+          <Input
+            value={fontSize}
+            className={classes.input}
+            id={FONT_INPUT}
+            onChange={handleInputChange}
+          ></Input>
         </Grid>
         <Grid item>
           {" "}
+          {/* TODO: wydzielić component */}
           <TextareaAutosize
             aria-label="message"
             value={message}
             rowsMin={8}
+            rowsMax={30}
             placeholder="wpisz swoją wiadomość"
             className={classes.textArea}
+            id={"firstInput"}
             onChange={handleInputChange}
+            style={{
+              fontSize: fontSize,
+            }}
           />
           <TextareaAutosize
             aria-label="encryptedMessage"
             rowsMin={8}
+            rowsMax={30}
             value={encryptedMessage}
             placeholder="zaszyfrowana wiadomość"
+            id={"secondInput"}
             className={classes.textArea}
+            style={{
+              fontSize: fontSize,
+            }}
           />
+          <TextareaAutosize
+            aria-label="encryptedMessage"
+            rowsMin={8}
+            rowsMax={30}
+            value={decryptedMessage}
+            placeholder="odszyfrowana wiadomość"
+            id={"thirdInput"}
+            className={classes.textArea}
+            style={{
+              fontSize: fontSize,
+            }}
+          />
+        </Grid>
+        <Grid>
+          <div>czas wykonania w ms: {timeToEncrypt} </div>
+        </Grid>
+        <Grid>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={handleClick}
+            style={{
+              marginTop: "20px",
+            }}
+          >
+            przelicz
+          </Button>
         </Grid>
       </Grid>
     </div>
